d0c2f208d3c14db04cff5ae18590dad64ee1fe6c
marinakeu <marina_keunecke@hotmail.com>
2019-05-30 16:01:01 -0300

    configurações iniciais

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "<sua-chave>",
-  authDomain: "<sua-chave>",
-  databaseURL: "<sua-chave>",
-  projectId: "<sua-chave>",
-  storageBucket: "<sua-chave>",
-  messagingSenderId: "<sua-chave>",
-  appId: "<sua-chave>"
+  apiKey: "AIzaSyDY_6gH6ZoCVY4uN78mivIZ5as85vhc8bQ",
+  authDomain: "burger-queen-bb2e0.firebaseapp.com",
+  databaseURL: "https://burger-queen-bb2e0.firebaseio.com",
+  projectId: "burger-queen-bb2e0",
+  storageBucket: "burger-queen-bb2e0.appspot.com",
+  messagingSenderId: "817989020287",
+  appId: "1:817989020287:web:8db3cdebb8318222"
 };
 
 firebase.initializeApp(config);
