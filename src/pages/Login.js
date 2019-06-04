36639539b942c0465517577e33eaef2699de8c9a
marinakeu <marina_keunecke@hotmail.com>
2019-06-04 18:42:50 -0300

    login - tentativa de redirecionamento

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,17 +3,22 @@ import logo from '../assets/img/logo.png';
 import firebase from '../firebaseConfig';
 import withFirebaseAuth from 'react-with-firebase-auth';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import { Button, Form, Container, Tabs, Tab} from 'react-bootstrap';
+import { Button, Form, Container, Tabs, Tab } from 'react-bootstrap';
 
 const firebaseAppAuth = firebase.auth();
+const database = firebase.firestore();
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
+    this.createUser = this.createUser.bind(this);
+    this.handleRedirect = this.handleRedirect.bind(this);
     this.state = {
+      displayName: "",
       email: "",
       password: "",
-      role: ""
+      role: "",
+      isLoggedIn: false
     };
   }
 
@@ -23,19 +28,33 @@ class Login extends React.Component {
     this.setState(newState);
   }
 
-  createUser = () => {
-    if (this.state.role === "Salão" || this.state.role === "Cozinha") {
-      this.props.createUserWithEmailAndPassword(this.state.email, this.state.password);
+  handleRedirect(user) {
+console.log('oi')
+console.log(user.uid);
+this.setState({isLoggedIn: true});
+  }
 
-    console.log(this.state.email);
-    console.log(this.state.password);
-    console.log(this.state.role);
-    console.log(this.props.user.uid);
+  createUser() {
+    if (this.state.role === "Salão" || this.state.role === "Cozinha") {
+      this.props.createUserWithEmailAndPassword(this.state.email, this.state.password, this.state.displayName)
+      console.log(this.props);
+      console.log(this.props.user.uid)
+      // database.collection("users").doc(uid).update({
+      //   displayName: 'marina',
+      //   role: 'Cozinha'
+      firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+          console.log('tem user');      
+          handleRedirect(user);             
+
+        } else {
+          console.log("User is signed out.")
+        }
+      });
+      
     } else {
       alert('insira');
     }
-    
-    
   };
 
   signIn = () => {
@@ -47,6 +66,8 @@ class Login extends React.Component {
   };
 
   render() {
+    const isLoggedIn = this.state.isLoggedIn;
+   
     return (
       <div>
         <Container className="Text-align Display-flex Full-size">
@@ -72,6 +93,11 @@ class Login extends React.Component {
               </Tab>
               <Tab className="White-border Tab-box" eventKey="home" title="CADASTRO">
                 <Form>
+                  <Form.Group controlId="formBasicNameSignUp">
+                    <Form.Control size="lg" value={this.state.displayName} type="text"
+                      placeholder="nome"
+                      onChange={(e) => this.handleChange(e, "displayName")} />
+                  </Form.Group>
                   <Form.Group controlId="formBasicEmailSignUp">
                     <Form.Control size="lg" value={this.state.email} type="email"
                       placeholder="e-mail"
@@ -83,9 +109,9 @@ class Login extends React.Component {
                       onChange={(e) => this.handleChange(e, "password")} />
                   </Form.Group>
                   <Form.Group controlId="exampleForm.ControlSelect1">
-                    <Form.Control className="Grey-text" size="lg" as="select" 
-                    value={this.state.role}
-                    onChange={(e) => this.handleChange(e, "role")} >
+                    <Form.Control className="Grey-text" size="lg" as="select"
+                      value={this.state.role}
+                      onChange={(e) => this.handleChange(e, "role")} >
                       <option>escolha uma função</option>
                       <option>Salão</option>
                       <option>Cozinha</option>
@@ -98,7 +124,9 @@ class Login extends React.Component {
               </Tab>
             </Tabs>
           </div>
-        </Container>
+        </Container>        
+        {isLoggedIn ? <Redirect to="/Salao"/> : <span>oi</span>}
+    
       </div >
     );
   }
