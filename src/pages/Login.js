e53d3af13d3fc2a8fe1e1f4547133fe2ef668c54
marinakeu <marina_keunecke@hotmail.com>
2019-06-04 11:20:30 -0300

    login - altera modal de cadastro para tabs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,9 +4,7 @@ import logo from '../assets/img/logo.png';
 import firebase from '../firebaseConfig';
 import withFirebaseAuth from 'react-with-firebase-auth';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-// import Form from 'react-bootstrap/Form';
-// import Button from 'react-bootstrap/Button';
-import { Button, Form, Container, Modal, ButtonToolbar } from 'react-bootstrap';
+import { Button, Form, Container, Modal, ButtonToolbar, Tabs, Tab, Nav } from 'react-bootstrap';
 import MyVerticallyCenteredModal from './components/MyVerticallyCenteredModal';
 
 const firebaseAppAuth = firebase.auth();
@@ -17,7 +15,7 @@ class Login extends React.Component {
     this.state = {
       email: "",
       senha: "",
-      modalShow: false
+
     };
   }
 
@@ -29,7 +27,7 @@ class Login extends React.Component {
 
   createUser = () => {
     this.props.createUserWithEmailAndPassword(this.state.email, this.state.senha);
-    this.setState({ modalShow: true });
+
     console.log(this.state.email);
     console.log(this.state.senha);
   };
@@ -42,35 +40,37 @@ class Login extends React.Component {
       });
   };
 
-  modalClose = () => this.setState({ modalShow: false });
-
   render() {
     return (
       <div>
         <Container className="Text-align Display-flex Full-size">
           <img className="Logo-img" src={logo} alt="Logo" />
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Control size="lg" value={this.state.email} type="email"
-                placeholder="e-mail"
-                onChange={(e) => this.handleChange(e, "email")} />
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Control size="lg" value={this.state.senha} type="password"
-                placeholder="senha"
-                onChange={(e) => this.handleChange(e, "senha")} />
-            </Form.Group>
-            <div className="Display-flex">
-              <Button variant="light" size="lg" onClick={this.signIn}>Entrar</Button>
-              <Button variant="dark" size="lg" onClick={this.createUser}>Cadastrar</Button>
-            </div>
-          </Form>
-
-          <MyVerticallyCenteredModal
-            show={this.state.modalShow}
-            onHide={() => this.modalClose()}
-          />
-
+          <div>
+            <Tabs className="Display-flex" defaultActiveKey="profile" id="uncontrolled-tab-example">
+              <Tab className="White-border" eventKey="home" title="LOGIN">
+                <Form>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Control size="lg" value={this.state.email} type="email"
+                      placeholder="e-mail"
+                      onChange={(e) => this.handleChange(e, "email")} />
+                  </Form.Group>
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Control size="lg" value={this.state.senha} type="password"
+                      placeholder="senha"
+                      onChange={(e) => this.handleChange(e, "senha")} />
+                  </Form.Group>
+                </Form>
+                <div className="Display-flex">
+                  <Button variant="light" size="lg" onClick={this.signIn}>Entrar</Button>
+                </div>
+              </Tab>
+              <Tab className="White-border" eventKey="profile" title="CADASTRO">
+                <div>
+                  <Button variant="dark" size="lg" onClick={this.createUser}>Cadastrar</Button>
+                </div>
+              </Tab>
+            </Tabs>
+          </div>
         </Container>
       </div >
     );
